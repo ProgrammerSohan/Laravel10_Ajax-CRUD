f9b5523d82e94e8d496fd4229cfba1d2db63c3fb
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-02-02 16:34:43 +0600

    design view-line awesome-p3_b

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,23 +10,63 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+
+
+    <title>Laravel Ajax Crud</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
 
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <div class="container">
+        <div class="row">
 
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+            <div class="col-md-2">     </div>
+                <div class="col-md-8">
+                    <h2 class="my-5 text-center">Laravel 9 Ajax Crud</h2>
+                    <div class="table-data">
+
+
+                        <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Action</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <th >1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>
+                <a href="" class="btn btn-success"> <i class="las la-edit"></i></a>
+                 <a href="" class="btn btn-danger"> <i class="las la-times"></i></a>
+                                </td>
+                              </tr>
+
+
+                            </tbody>
+                          </table>
+
+
+                    </div>
+
+
+
+
+                </div>
+        </div>
+
+
+    </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
