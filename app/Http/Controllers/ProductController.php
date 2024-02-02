d58cfb89d9b0d16f51636290fe030a4a81810fae
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-02-02 18:11:53 +0600

    insert data-hide modal-reset form-p7

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -26,6 +27,15 @@ class ProductController extends Controller
               'price.required'=>'Price is Required',
           ]
       );
+      $product = new Product();
+      $product->name = $request->name;
+      $product->price = $request->price;
+      $product->save();
+
+      return response()->json([
+         'status'=>'success',
+     ]);
+
   }
 
 }
